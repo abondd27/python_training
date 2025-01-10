9f5bea0012f67cfe339e1406f3b39ae754599546
abondareva <abondareva@ptsecurity.com>
2025-01-10 11:08:02 +0700

    Доработаны тесты для удаления  , модификации, добавления групп и контактов путем сравнения списков

--- fixture/group.py
+++ fixture/group.py
@@ -1,6 +1,7 @@
 from selenium.webdriver.common.by import By
-
-
+from model.group import Group
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 class GroupHelper:
 
     def __init__(self, app):
@@ -68,3 +69,14 @@ class GroupHelper:
         wd = self.app.wd
         self.open_groups_page()
         return len(wd.find_elements(By.NAME, "selected[]"))
+
+
+    def get_group_list(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        groups = []
+        for element in wd.find_elements(By.CSS_SELECTOR, "span.group"):
+            text = element.text
+            id = element.find_element(By.NAME, "selected[]"). get_attribute("value")
+            groups.append(Group(name=text, id=id))
+        return groups
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -1,19 +1,34 @@
 from model.group import Group
 
 
-def test_modify_first_group(app):
-    if app.group.count() == 0:
-        app.group.create(Group(name="Test277", header="Test277", footer="Test277"))
-    app.group.modify_first_group(Group(name="Test2", header="Test2", footer="Test2"))
+#def test_modify_first_group(app):
+#    old_groups = app.group.get_group_list()
+#    if app.group.count() == 0:
+#        app.group.create(Group(name="Test277", header="Test277", footer="Test277"))
+#    app.group.modify_first_group(Group(name="Test2", header="Test2", footer="Test2"))
+#    new_groups = app.group.get_group_list()
+#    assert len(old_groups)  == len(new_groups)
 
 
 def test_modify_first_group_name(app):
-    app.group.modify_first_group(Group(name="New Group"))
+    old_groups = app.group.get_group_list()
+    group = Group(name="New Group")
+    group.id = old_groups[0].id
+    app.group.modify_first_group(group)
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
+    old_groups[0] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+
+#def test_modify_first_group_header(app):
+#    old_groups = app.group.get_group_list()
+#    app.group.modify_first_group(Group(header="New Header"))
+#    new_groups = app.group.get_group_list()
+#    assert len(old_groups) == len(new_groups)
 
 
-def test_modify_first_group_header(app):
-    app.group.modify_first_group(Group(header="New Header"))
-
-
-def test_modify_first_group_footer(app):
-    app.group.modify_first_group(Group(footer="New Footer"))
+#def test_modify_first_group_footer(app):
+#   old_groups = app.group.get_group_list()
+#   app.group.modify_first_group(Group(footer="New Footer"))
+#   new_groups = app.group.get_group_list()
+#    assert len(old_groups) == len(new_groups)
